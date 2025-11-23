9abdac1d7be7fe851e8297b4429d616777b55665
Oscar Albán <oscar.alban@epn.edu.ec>
2025-11-23 18:49:09 -0500

    Indice Invertido Completo con frecuencia

--- indice_invertido.py
+++ indice_invertido.py
@@ -1,28 +1,24 @@
-import pandas as pd
-
 def construir_indice(df_tokens_preprocesados):
 
-
-    df_tokens = df_tokens_preprocesados.copy()
-    df_tokens["id"] = df_tokens.index
-
+    df_tokens = df_tokens_preprocesados.reset_index().copy()
+ 
     df_tokens = (
-        df_tokens[["id", "tokens"]]
+        df_tokens[["doc_id", "tokens"]]
         .explode("tokens")
         .rename(columns={"tokens": "termino"})
     )
 
     df_tokens.dropna(subset=["termino"], inplace=True)
 
-    # Calcular la frecuencia TF por término y documento
     df_frecuencias = (
-        df_tokens.groupby(["termino", "id"])
+        df_tokens.groupby(["termino", "doc_id"])
         .size()
         .reset_index(name="frecuencia")
     )
 
     indice_invertido = (
-        df_frecuencias.groupby("termino")[["id", "frecuencia"]]
+        df_frecuencias
+        .groupby("termino")[["doc_id", "frecuencia"]]
         .apply(lambda x: list(map(tuple, x.values)))
         .to_dict()
     )
