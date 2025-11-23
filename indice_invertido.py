4f3fa831575eb8b8e34b4d76355ef2d34c527f97
Oscar Albán <oscar.alban@epn.edu.ec>
2025-11-23 18:25:19 -0500

    indice invertifo

--- indice_invertido.py
+++ indice_invertido.py
@@ -2,26 +2,29 @@ import pandas as pd
 
 def construir_indice(df_tokens_preprocesados):
 
+
+    df_tokens = df_tokens_preprocesados.copy()
+    df_tokens["id"] = df_tokens.index
+
     df_tokens = (
-        df_tokens_preprocesados[['tokens_finales', 'doc_id']]
-        .explode('tokens_finales')
-        .rename(columns={'tokens_finales': 'termino'})
+        df_tokens[["id", "tokens"]]
+        .explode("tokens")
+        .rename(columns={"tokens": "termino"})
     )
 
-    df_tokens.dropna(subset=['termino'], inplace=True)
+    df_tokens.dropna(subset=["termino"], inplace=True)
 
-    # 2. Calcular la frecuencia (TF)
+    # Calcular la frecuencia TF por término y documento
     df_frecuencias = (
-        df_tokens.groupby(['termino', 'doc_id'])
+        df_tokens.groupby(["termino", "id"])
         .size()
-        .reset_index(name='frecuencia')
+        .reset_index(name="frecuencia")
     )
 
-    # 3. Construir el diccionario final
     indice_invertido = (
-        df_frecuencias.groupby('termino')[['doc_id', 'frecuencia']]
-        .apply(lambda x: list(map(tuple, x.values))) 
+        df_frecuencias.groupby("termino")[["id", "frecuencia"]]
+        .apply(lambda x: list(map(tuple, x.values)))
         .to_dict()
     )
-    
+
     return indice_invertido
